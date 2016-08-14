a7c2faf62686f70f2a587f64af904ad87007e515
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2016-08-15 03:12:54 +0530

    whole game itself

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+	
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+
+	public bool doRotate = true;
+	public bool doMotion = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (doRotate) {
+			// rotation code
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+		}
+
+		if (doMotion) {
+			// translation code
+			this.gameObject.transform.Translate (Vector3.up * motionMagnitude * Mathf.Cos (Time.timeSinceLevelLoad));
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Horizontal Input = "+ Input.GetAxis("Horizontal"));
+	}
+}
